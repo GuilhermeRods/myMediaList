8ebb74439c5a5f4b77ef93b966f7afd0dec5e785
guilhermerods <guilherme.rodrigues@nave.rs>
2020-04-29 03:16:03 -0300

    adiciona rota de exemplo

--- src/controllers/index.js
+++
@@ -1,7 +0,0 @@
-const Router = require("koa-router");
-
-const rout = new Router();
-
-rout.get('/',ctx => ctx.body="Bombouu");
-
-module.exports = rout.routes();
---
+++ src/routes/medias-router.js
@@ -0,0 +1,8 @@
+import Router from 'koa-router'
+
+import mediasController from '../controllers/mediasController'
+
+const router = new Router()
+
+
+export default router.routes()

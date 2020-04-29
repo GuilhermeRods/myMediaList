99c2ab8d58796bbb5293f7a58f8c3bd534d16e3e
guilhermerods <guilherme.rodrigues@nave.rs>
2020-04-29 19:46:17 -0300

    add folder api

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import 'babel-polyfill'
-import app from './server'
-import { PORT } from './config'
-
-app.listen(PORT, () => console.log(`Server running at ${PORT}`))
-
-export default app
--- src/routes/medias-router.js
+++
@@ -1,7 +0,0 @@
-import Router from 'koa-router'
-
-import mediasController from '../controllers/mediasController'
-
-const router = new Router()
-
-export default router.routes()

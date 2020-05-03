4ad288054751b9df1f6386b839173f468e4fc7ee
Aldair Rodrigues <ifsulaldair@gmail.com>
2020-05-03 18:20:59 -0300

    tipo de dados corrigido em genres"

--- backend/src/database/migrations/20200503013823_create_genres.js
+++ backend/src/database/migrations/20200503013823_create_genres.js
@@ -1,6 +1,6 @@
 export const up = knex => knex.schema.createTable('genres', table => {
   table.increments()
-  table.integer('name').notNullable()
+  table.string('name').notNullable()
   table.timestamps(true, true)
   table.timestamp('deleted_at')
 

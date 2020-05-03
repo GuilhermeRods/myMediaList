d9b96aeb27cf2e859b69fe3109cbbfef25b4cc03
Aldair Rodrigues <ifsulaldair@gmail.com>
2020-05-03 04:18:00 -0300

    add constraints 'unique' on email of users

--- backend/src/database/migrations/20200502231019_create_users.js
+++ backend/src/database/migrations/20200502231019_create_users.js
@@ -2,7 +2,7 @@
 export const up = knex => knex.schema.createTable('users', table => {
   table.uuid('id').primary()
   table.string('name').notNullable()
-  table.string('email').notNullable().unique()
+  table.unique('email').notNullable()
   table.string('password').notNullable()
   table.timestamps(true, true)
   table.timestamp('deleted_at')

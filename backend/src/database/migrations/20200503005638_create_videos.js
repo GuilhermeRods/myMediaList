cb07b59ca308c595dbb2855f838dedfeec50577d
Aldair Rodrigues <ifsulaldair@gmail.com>
2020-05-03 04:07:48 -0300

    add constraints in videos

--- backend/src/database/migrations/20200503005638_create_videos.js
+++ backend/src/database/migrations/20200503005638_create_videos.js
@@ -1,11 +1,11 @@
 export const up = knex => knex.schema.createTable('videos', table => {
   table.increments()
-  table.string('title')
-  table.integer('video_number')
+  table.string('title').notNullable()
+  table.integer('video_number').notNullable()
   table.timestamps(true, true)
   table.timestamp('deleted_at')
 
-  table.integer('media_id')
+  table.integer('media_id').notNullable()
 
   table.foreign('media_id').references('id').inTable('medias')
 })

3b2c511208e09088923af52accd3769608f9e921
Aldair Rodrigues <ifsulaldair@gmail.com>
2020-05-03 04:02:38 -0300

    add constraints in user_video

--- backend/src/database/migrations/20200503014833_create_user_video.js
+++ backend/src/database/migrations/20200503014833_create_user_video.js
@@ -1,11 +1,11 @@
 
 export const up = knex => knex.schema.createTable('user_video', table => {
 
-  table.integer('stopped_watching_on')
-  table.integer('total_views')
+  table.integer('stopped_at').notNullable()
+  table.integer('views')
 
-  table.integer('user_id')
-  table.integer('video_id')
+  table.integer('user_id').notNullable()
+  table.integer('video_id').notNullable()
 
   table.foreign('user_id').references('id').inTable('users')
   table.foreign('video_id').references('id').inTable('videos')

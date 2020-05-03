3698d24d552a999f977d6be1555707c66ed4fe02
Aldair Rodrigues <ifsulaldair@gmail.com>
2020-05-03 03:31:53 -0300

    Add constraints in users

--- backend/src/database/migrations/20200502231019_create_users.js
+++ backend/src/database/migrations/20200502231019_create_users.js
@@ -1,9 +1,9 @@
 
 export const up = knex => knex.schema.createTable('users', table => {
   table.increments()
-  table.string('name')
-  table.string('email')
-  table.string('password')
+  table.string('name').notNullable()
+  table.string('email').notNullable()
+  table.string('password').notNullable()
   table.timestamps(true, true)
   table.timestamp('deleted_at')
 })

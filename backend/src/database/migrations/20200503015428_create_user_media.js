537b807cf31ce33e636e294d5439054cb7edb62a
Aldair Rodrigues <ifsulaldair@gmail.com>
2020-05-03 03:58:25 -0300

    add constraints in media_type

--- backend/src/database/migrations/20200503015428_create_user_media.js
+++ backend/src/database/migrations/20200503015428_create_user_media.js
@@ -5,8 +5,8 @@ export const up = knex => knex.schema.createTable('user_media', table => {
   table.timestamps(true, true)
   table.timestamp('deleted_at')
 
-  table.integer('user_id')
-  table.integer('media_id')
+  table.integer('user_id').notNullable()
+  table.integer('media_id').notNullable()
 
   table.foreign('user_id').references('id').inTable('users')
   table.foreign('media_id').references('id').inTable('medias')

d9bf9c7e65cdb6f9d888c1e29090cc8eb909b158
guilhermerods <guilherme.rodrigues@nave.rs>
2020-05-04 03:34:42 -0300

    add database connection

--- backend/src/database/knexfile.js
+++ backend/src/database/knexfile.js
@@ -1,18 +1,28 @@
 import { DATABASE } from '../config'
 import path from 'path'
 
-export const 
-  development = {
-    client: 'pg',
-    connection: DATABASE,
-    migrations: {
-      directory: path.resolve(__dirname, 'migrations')
-    },
-    useNullAsDefault: true
-  }
+export const development = {
+  client: 'pg',
+  connection: DATABASE,
+  migrations: {
+    directory: path.resolve(__dirname, 'migrations')
+  },
+  useNullAsDefault: true
+}
 
+export const test = {
+  client: 'sqlite3',
+  connection: {
+    filename: path.resolve(__dirname, 'db_test.sqlite3')
+  },
+  migrations: {
+    directory: path.resolve(__dirname, 'migrations')
+  },
+  useNullAsDefault: true
+}
 const knex = {
-    development
-  }
+  development,
+  test
+}
 
 export default knex

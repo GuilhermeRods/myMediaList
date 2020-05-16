9733c493d60438105add9f5bd7437f6d2d102fc0
Aldair Rodrigues <ifsulaldair@gmail.com>
2020-05-16 01:38:27 -0300

    fix/add-services

--- backend/src/services/usersServices.js
+++ backend/src/services/usersServices.js
@@ -2,3 +2,6 @@ import { connectionDev } from '../database/connect'
 
 export const insertUserOnDB = async fields =>
   connectionDev('users').returning('*').insert(fields)
+
+export const getUser = async fields =>
+  connectionDev('users').select('id').where(fields)

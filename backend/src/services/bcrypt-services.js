90f18c4144b2e395ba35b29330283763460b4924
Aldair Rodrigues <ifsulaldair@gmail.com>
2020-05-16 02:07:33 -0300

    fix/function-correction

--- backend/src/services/bcrypt-services.js
+++ backend/src/services/bcrypt-services.js
@@ -5,7 +5,7 @@ const saltRounds = 10
 const saltKeys = '02Hm31nHC'
 
 export const generatePasswordHashed = password =>
-  bcrypt.hashSync(password, saltRounds, saltKeys)
+  bcrypt.hashSync(`${password}${saltKeys}`, saltRounds)
 
 export const verifyPassword = (passwordToCompare, password) =>
   bcrypt.compareSync(`${passwordToCompare}${saltKeys}`, password)

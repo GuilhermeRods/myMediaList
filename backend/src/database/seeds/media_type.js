e47b806b6a7d2dcfbc3e4edf1ec1914618cde157
Aldair Rodrigues <ifsulaldair@gmail.com>
2020-05-30 07:34:47 -0300

    fix/removing field

--- backend/src/database/seeds/media_type.js
+++ backend/src/database/seeds/media_type.js
@@ -2,17 +2,14 @@ export const seed = async knex => {
   await knex('media_type').del()
   await knex('media_type').insert([
     {
-      id: knex.raw('uuid_generate_v4()'),
       type: 'Animes'
     },
 
     {
-      id: knex.raw('uuid_generate_v4()'),
       type: 'Filmes'
     },
 
     {
-      id: knex.raw('uuid_generate_v4()'),
       type: 'Series'
     },
   ])

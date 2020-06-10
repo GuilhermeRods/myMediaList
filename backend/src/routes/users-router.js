1fb7215f927f02d682fed5b4e217ce1c3737a628
joezersmaniotto <joezersmaniotto5@gmail.com>
2020-06-09 21:15:49 -0300

    Correcao rotas com ID

--- backend/src/routes/users-router.js
+++ backend/src/routes/users-router.js
@@ -5,8 +5,8 @@ import UsersController from '../controllers/usersController'
 const router = new Router()
 
 router.post('/users/signup', UsersController.create)
+router.get('/users', UsersController.index)
 router.get('/users/:id', UsersController.show)
 router.delete('/users/:id', UsersController.destroy)
-router.get('/users', UsersController.index)
 
 export default router.routes()

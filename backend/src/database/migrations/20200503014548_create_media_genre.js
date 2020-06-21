0e9d1e47afc04b0f41c96ca062263d8d84af7b2b
guilhermerods <rodsguilherme@gmail.com>
2020-06-21 05:34:57 -0300

    add genre api id in migration

--- backend/src/database/migrations/20200503014548_create_media_genre.js
+++ backend/src/database/migrations/20200503014548_create_media_genre.js
@@ -1,12 +1,17 @@
 export const up = knex =>
   knex.schema.createTable("media_genres", table => {
     table.uuid("id").defaultTo(knex.raw("uuid_generate_v4()")).primary()
-    table.timestamps(true, true)
-    table.timestamp("deleted_at")
 
     table.uuid("media_id").notNullable()
-    table.uuid("genre_id").notNullable()
+    table.uuid("genre_id")
+    table.integer("genre_api_id").notNullable()
+    table.integer("media_api_id").notNullable()
 
+    table.timestamp("deleted_at")
+    table.foreign("media_api_id").references("media_api_id").inTable("medias")
+    table.foreign("genre_api_id").references("genre_api_id").inTable("genres")
+
+    table.timestamps(true, true)
     table.foreign("media_id").references("id").inTable("medias")
     table.foreign("genre_id").references("id").inTable("genres")
   })

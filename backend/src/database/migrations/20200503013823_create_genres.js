c2164263450aadefb6609f7d9bb7353227a2cb82
guilhermerods <rodsguilherme@gmail.com>
2020-06-21 05:12:45 -0300

    refactor genres to accept data of api

--- backend/src/database/migrations/20200503013823_create_genres.js
+++ backend/src/database/migrations/20200503013823_create_genres.js
@@ -1,7 +1,10 @@
 export const up = knex =>
   knex.schema.createTable("genres", table => {
     table.uuid("id").defaultTo(knex.raw("uuid_generate_v4()")).primary()
-    table.string("name").notNullable()
+    table.integer("genre_api_id").unique().notNullable()
+    table.string("name_es").notNullable()
+    table.string("name_pt").notNullable()
+    table.string("slug").notNullable()
     table.timestamps(true, true)
     table.timestamp("deleted_at")
   })
